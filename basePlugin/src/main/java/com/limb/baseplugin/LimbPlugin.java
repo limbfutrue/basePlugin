8e5c1f9c31886126fc20cce409781f1ced9f15ad
sjyh-libaoming <13716572013@163.com>
2023-03-13 16:23:15 +0800

    first commit plugin project

---
+++ basePlugin/src/main/java/com/limb/baseplugin/LimbPlugin.java
@@ -0,0 +1,13 @@
+package com.limb.baseplugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class LimbPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        System.out.println("----------------------------------------------");
+        System.out.println("----------------------------------------------");
+        System.out.println("----------------------------------------------");
+    }
+}
